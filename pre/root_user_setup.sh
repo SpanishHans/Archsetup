57f3c2da3410b32f51e028e46e25f29805088f28
Juan José Martínez <89874590+SpanishHans@users.noreply.github.com>
2025-01-16 21:43:37 +0000

    superuser_setup

--- pre/root_user_setup.sh
+++ pre/root_user_setup.sh
@@ -22,7 +22,8 @@ root_password_prompt () {
     eval "$pass='$root_password'"
 }
 
-root_setup () {
+superuser_setup () {
+    clean
     pause_script "Entered root user setup!" "The following section will help you configure the password for the root user. This script does not create more users during install, they are created after install if you so desire.
 
 With this in mind, DONT LOOSE YOUR ROOT PASSWORD, or you wont be able to login after install."
