ba2da7d70a40c3380efca5c952a40ac9c0300fb0
Juan José Martínez <89874590+SpanishHans@users.noreply.github.com>
2025-01-16 23:26:49 +0000

    dhsfdgs

--- pre/disk_setup.sh
+++ pre/disk_setup.sh
@@ -53,13 +53,14 @@ With this in mind, lets pick between sane defaults or full custom mode.'
 }
 
 default_route() {
-    continue_script "Default route" "You chose to use the default route"
+    # continue_script "Default route" "You chose to use the default route"
     format_and_partition_disks
     set_filesystem_for_partitions
 }
 
 full_custom_route() {
     continue_script "Custom route" "You chose to use the custom route"
+    pause_script "" "continue"
 }
 
 format_and_partition_disks() {
