8b241600771960c4219db4d914f01318cc260e21
Juan José Martínez <89874590+SpanishHans@users.noreply.github.com>
2025-01-17 04:53:36 +0000

    \n

--- post/users.sh
+++ post/users.sh
@@ -165,7 +165,7 @@ delete_user() {
 
     local user_list=$(printf "%s\n" "${menu_items[@]}")
     
-    input_text username username_status "Delete user" "Menu for deleting a user. This will DELETE THEIR FILES! $$user_list" "Enter the username to delete: "
+    input_text username username_status "Delete user" "Menu for deleting a user. This will DELETE THEIR FILES! \n$user_list" "Enter the username to delete: "
     
 
 
