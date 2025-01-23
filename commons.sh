3734875ab996858ddff97bc156f5a6ca65e9a2e2
Juan José Martínez <89874590+SpanishHans@users.noreply.github.com>
2025-01-23 15:27:55 +0000

    --tailbox "$combined_log" \

--- commons.sh
+++ commons.sh
@@ -175,7 +175,7 @@ live_command_output() {
     # Show the dialog live
     dialog \
         --backtitle "Command Execution Viewer" \
-        --progressbox "$combined_log" \
+        --tailbox "$combined_log" \
         "$full_height" "$full_width" 2>&1 >/dev/tty
 
     # Wait for all commands to finish
