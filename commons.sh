7ae5fbcf6d23895c10b3a9b0778cd5ece497e283
Juan José Martínez <89874590+SpanishHans@users.noreply.github.com>
2025-02-07 23:12:58 -0500

    logfs

--- commons.sh
+++ commons.sh
@@ -229,7 +229,7 @@ live_command_output() {
 
         if [ $exit_code -eq 0 ]; then
             terminal_title "Done, continuing to next step!" >> "$combined_log"
-            terminal_title "\n\nread the logs for this operation on $combined_log" >> "$combined_log"
+            terminal_title "read the logs for this operation on $combined_log" >> "$combined_log"
             sleep 2
             killall dialog
         fi
