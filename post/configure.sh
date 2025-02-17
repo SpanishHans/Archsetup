4cd074c87e0dd5afc2b942b9d681f5dccde734ad
Juan José Martínez <89874590+SpanishHans@users.noreply.github.com>
2025-02-17 02:57:55 +0000

    dgag

--- post/configure.sh
+++ post/configure.sh
@@ -43,7 +43,7 @@ fi
 configure_menu () {
     local user="sysadmin"
 
-    input_pass "$user" pass "Password check for $user" "PKBUILD builds can not be done with root, therefore we shall use the user: 
+    input_pass "$user" "pass" "Password check for $user" "PKBUILD builds can not be done with root, therefore we shall use the user: 
 
 - $user"
 
