d4e37dd054d0bece62c7f9469572ad2aca8d0b35
Juan José Martínez <89874590+SpanishHans@users.noreply.github.com>
2025-02-17 02:32:20 +0000

    adgadg

--- post/0_users/users.sh
+++ post/0_users/users.sh
@@ -63,9 +63,9 @@ check_pass() {
     local user="$1"
     local prompt="$2"
 
-    input_pass pass "sysadmin" "Password check" "Checking password for '$user'."
+    input_pass pass "$user" "Password check" "Checking password for '$user'."
 
-    continue_script 2 "$user" "Password entered is: $pass"
+    continue_script 2 "$user" pass
 
     while true; do
         echo "$pass" | sudo -S -u "$user" whoami > /dev/null 2>&1
