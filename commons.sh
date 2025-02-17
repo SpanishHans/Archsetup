a554287afc2bdb910226b9d448d3e1d18a7afb1c
Juan José Martínez <89874590+SpanishHans@users.noreply.github.com>
2025-02-17 16:56:52 +0000

    Ok, Continue

--- commons.sh
+++ commons.sh
@@ -168,7 +168,8 @@ scroll_window_output() {
     dialog --backtitle "Viewing $file" \
         --title "$file on logs viewer" \
         --exit-label "Cancel" \
-        --ok-label "Ok, Continue" \
+        --extra-button \
+        --extra-label "Ok, Continue" \
         --textbox "$temp_file" \
         "${full_height:-20}" "${full_width:-70}"
 
