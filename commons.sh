97b9d453979e6034400b73511a24a0b018e3f9dc
Juan José Martínez <89874590+SpanishHans@users.noreply.github.com>
2025-02-17 02:10:44 +0000

    continue_script 2 "" "Debug: Password entered is: $password1"

--- commons.sh
+++ commons.sh
@@ -310,6 +310,7 @@ input_pass() {
         if [ "$password1" != "$password2" ]; then
             pause_script "Password Error" "Passwords for '$user' do not match. Please try again."
         else
+            continue_script 2 "" "Debug: Password entered is: $password1"
             eval "$pass=\"$password1\""
             break
         fi
