71a61ef07dd685deeb612085fbc4514764b97d4d
Juan José Martínez <89874590+SpanishHans@users.noreply.github.com>
2025-02-17 16:54:58 +0000

    return_value

--- post/4_software/aur.sh
+++ post/4_software/aur.sh
@@ -77,7 +77,7 @@ install_without_paru() {
 
     if ! ls $build_path/*.pkg.tar.zst &>/dev/null; then
         local commands_to_run=()
-        scroll_window_output "Viewing PKGBUILD for $package_name" "$build_path/PKGBUILD"
+        scroll_window_output return_value "Viewing PKGBUILD for $package_name" "$build_path/PKGBUILD"
         pause_script "" "scroll returned $return_value"
         local commands_to_run+=("cd $build_path && makepkg -s -r -c --noconfirm")
         live_command_output "$bui_user" "$bui_pass" "yes" "Building and installing $package_name" "${commands_to_run[@]}"
