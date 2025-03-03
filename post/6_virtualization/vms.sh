262b4a03707abc843712adfa3d149f963de513a0
Juan José Martínez <89874590+SpanishHans@users.noreply.github.com>
2025-03-03 01:47:48 +0000

    libvirt to vms

--- post/6_virtualization/vms.sh
+++ post/6_virtualization/vms.sh
@@ -18,7 +18,49 @@ source ./commons.sh
 source ./post/0_users/users.sh
 source ./post/4_software/pacman.sh
 source ./post/4_software/aur.sh
-source ./post/6_virtualization/libvirt.sh
+
+check_kvm() {
+    virtualization_ok=""
+    kvm_ok=""
+
+    if LC_ALL=C.UTF-8 lscpu | grep -q 'Virtualization'; then
+        virtualization_ok='Virtualization supported: YES';
+    else
+        virtualization_ok='Virtualization supported: NO';
+        exit 1;
+    fi
+
+    if zgrep -q CONFIG_KVM= /proc/config.gz; then
+        kvm_ok='KVM enabled: YES';
+    else
+        kvm_ok='KVM enabled: NO';
+        exit 1;
+    fi
+
+    continue_script 2 "Virt-Manager" "Virt manager initial check completed:
+
+1) KVM. --> $kvm_ok
+2) Virtualization --> $virtualization_ok"
+}
+
+configure_libvirt() {
+    local libvirt_user="$1"
+    install_pacman_packages libvirt dnsmasq virt-manager
+
+    check_kvm
+
+    local commands_to_run=()
+    commands_to_run+=("usermod -aG libvirt $libvirt_user")
+
+    commands_to_run+=("echo \"unix_sock_group = 'libvirt' \" | tee -a /etc/libvirt/libvirtd.conf")
+    commands_to_run+=("echo \"unix_sock_rw_perms = '0770' \" | tee -a /etc/libvirt/libvirtd.conf")
+
+    commands_to_run+=("systemctl enable libvirtd")
+    commands_to_run+=("systemctl restart libvirtd")
+    live_command_output "" "" "Installing dependencies" "${commands_to_run[@]}"
+
+    continue_script 2 "Libvirt" "Libvirt has been setup correctly!"
+}
 
 hypervisors_menu() {
     local title='Type 2 hypervisors.'
